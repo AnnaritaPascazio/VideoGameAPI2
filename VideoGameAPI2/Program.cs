44a55bd328c84fd4c351ab1c748677afb45af6be
Alexismoon23 <apascazio@studenti.apuliadigitalmaker.it>
2025-03-09 21:10:13 +0100

    creating-our-database

--- VideoGameAPI2/Program.cs
+++ VideoGameAPI2/Program.cs
@@ -1,4 +1,6 @@
 using Scalar.AspNetCore;
+using VideoGameAPI2.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<VideoGameDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
